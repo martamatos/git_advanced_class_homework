fa72d0512d3bcb1e835bc1e31959ba00f64ee155
Kseniia Zar <kseniiazar@gmail.com>
2019-06-03 10:48:59 +0200

    added division function

--- math.js
+++ math.js
@@ -1,14 +1,18 @@
-const addNumbers = function(a,b) {
-  return a+b;
+const addNumbers = function(a, b) {
+  return a + b;
 };
 
-
-const subtractNumbers = function(a,b) {
-  return a-b;
+const subtractNumbers = function(a, b) {
+  return a - b;
 };
-
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
 
+//function that divides two numbers, added on June 3, 2019
+const divideNumbers = function(a, b) {
+  return a / b;
+};
+
+console.log(divideNumbers());
