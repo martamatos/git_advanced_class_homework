07f7b14771bc78a14a2dfa10085dd3fa36fc4cd4
nmoskaleva <nsmoskaleva@gmail.com>
2019-06-04 16:05:02 +0200

    added a division function

--- math.js
+++ math.js
@@ -1,12 +1,12 @@
-const addNumbers = function(a,b) {
-  return a+b;
+//in the file math.js add a function to divide two numbers at the end of the file. the function should take two numbers as arguments (e.g., a and b) and return the result of their division (e.g. a/b). Commit your changes;
+
+const addNumbers = function(a, b) {
+  return a + b;
 };
 
-
-const subtractNumbers = function(a,b) {
-  return a-b;
+const subtractNumbers = function(a, b) {
+  return a - b;
 };
-
 
 console.log(addNumbers());
 
@@ -14,3 +14,8 @@ console.log(subtractNumbers());
 
 console.log("hello");
 
+const divideNumbers = function(a, b) {
+  return a / b;
+};
+
+console.log(divideNumbers(10, 5));
