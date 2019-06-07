43cbfb8cc51319244f8f7c53e79c3e6b4d53a754
afshin <44581882+Afshinrommel@users.noreply.github.com>
2019-06-07 04:03:00 +0200

    Update math.js

--- math.js
+++ math.js
@@ -1,16 +1,17 @@
-const addNumbers = function(a,b) {
-  return a+b;
-};
-
-
-const subtractNumbers = function(a,b) {
-  return a-b;
-};
-
-
-console.log(addNumbers());
-
-console.log(subtractNumbers());
-
-console.log("hello");
+  const addNumbers = function(a,b) {
+    return a+b;
+  };
+  
+  const subtractNumbers = function(a,b) {
+    return a-b;
+  };
+  
+  const divideNumbers = function(a,b){
+      return a/b;
+  }
+  
+  console.log(addNumbers(7,8));
+  console.log(subtractNumbers(17,58));
+  console.log(divideNumbers(97,81));
+  console.log("hello");
 
