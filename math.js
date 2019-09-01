abbeca928618e49a9fab7379997d49a4ce6636ef
lil2400 <il.memedi@gmail.com>
2019-09-01 22:40:41 +0200

    Added dividing function

--- math.js
+++ math.js
@@ -7,10 +7,14 @@ const subtractNumbers = function(a,b) {
   return a-b;
 };
 
+const divide = function (a,b) {
+	return a / b;
+}
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
 
-console.log("hello");
+console.log(divide());
+
 
