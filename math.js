74d1def8930d41792bf58a3934d870d5d89b1a5a
Simona239 <simona.mikelionyte@gmail.com>
2019-11-17 09:33:06 +0100

    division function added

--- math.js
+++ math.js
@@ -8,9 +8,15 @@ const subtractNumbers = function(a,b) {
 };
 
 
+const divideNumbers = function(a,b) {
+  return a/b;
+};
+
 console.log(addNumbers());
 
 console.log(subtractNumbers());
+
+console.log(divideNumbers());
 
 console.log("hello");
 
